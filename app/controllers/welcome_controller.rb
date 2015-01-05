b671247bdd2f554c598603d999f316e7d3fe48d8
pam- <pam.assogba@gmail.com>
2015-01-05 17:46:08 -0500

    demo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 	def index
 		@date = Date.today.to_formatted_s(:long_ordinal)
 		@friday = Time.now.strftime("%A") == 'Friday'
-
+		@today = Time.now.strftime("%A")
 		@todos = Todo.where(date: Date.today.to_s)
 	end
 end 
